f78ab6e763f1305cc2711f4f710dde8d9bc5f8fd
Annkatrin Bressin <bressin@schlumpfine.molgen.mpg.de>
2019-01-20 14:00:12 +0100

    add E.coli test dataset to paper

--- paper/figure_S4/figure_S4.sh
+++ paper/figure_S4/figure_S4.sh
@@ -20,6 +20,16 @@ if [ ! -f $scriptDir/../data/test/TriPepSVM/NRBP_9606.TriPepSVM.pred.txt ];then
 	TriPepSVM.sh -i $scriptDir/../data/test/NRBP_9606.fasta -o $scriptDir/../data/test/TriPepSVM -id 9606 -pos 1.8 -neg 0.2 -thr 0.68 -m trainData
 fi
 
+#####  get performance: e.coli
+if [ ! -f $scriptDir/../data/test/TriPepSVM/RBP_561.TriPepSVM.pred.txt ];then
+	TriPepSVM.sh -i $scriptDir/../data/test/RBP_561.fasta -o $scriptDir/../data/test/TriPepSVM -id 561 -pos 1.8 -neg 0.2 -thr 0.28 -m trainData
+fi
+
+if [ ! -f $scriptDir/../data/test/TriPepSVM/NRBP_561.TriPepSVM.pred.txt ];then
+	TriPepSVM.sh -i $scriptDir/../data/test/NRBP_561.fasta -o $scriptDir/../data/test/TriPepSVM -id 561 -pos 1.8 -neg 0.2 -thr 0.28 -m trainData
+fi
+
+
 ##### data processing
 
 ### RNAPred
@@ -31,6 +41,12 @@ done
 
 RNAPred_Cutoff=-0.18
 for file in $scriptDir/../data/test/RNAPred/*590*.temp;do
+	base=${file%.*} 	
+	awk -v OFS="\t" -F"\t" '{ if( $2 >= '$RNAPred_Cutoff' ) print $1,$2,"RNA-binding protein"; else print $1,$2,"Non RNA-binding protein";}' $file > $base.txt  
+done
+
+RNAPred_Cutoff=0.35
+for file in $scriptDir/../data/test/RNAPred/*561*.temp;do
 	base=${file%.*} 	
 	awk -v OFS="\t" -F"\t" '{ if( $2 >= '$RNAPred_Cutoff' ) print $1,$2,"RNA-binding protein"; else print $1,$2,"Non RNA-binding protein";}' $file > $base.txt  
 done
