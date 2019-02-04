bd4cdc7b95647343bafc5a6ff83f499a2d3ebdb9
Roman Schulte-Sasse <sasse@molgen.mpg.de>
2019-02-04 14:53:17 +0100

    Updated figures for revision (figures 2, 3, S5, S8).

--- paper/figure_S8/predict_all_species.sh
+++ paper/figure_S8/predict_all_species.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Evaluate on human proteome (for e.coli and salmonella)
 mkdir ../data/cross_species/561_on_9606_proteome
-bash ../../TriPepSVM.sh -i ../data/proteome_9606.fasta -o ../data/cross_species/561_on_9606_proteome/ -id 561 -pos 1.8 -neg 0.2 -m trainData
+bash ../../TriPepSVM.sh -i ../data/proteome_9606.fasta -o ../data/cross_species/561_on_9606_proteome/ -id 561 -pos 1.0 -neg 0.1 -m trainData
 
 mkdir ../data/cross_species/590_on_9606_proteome
 bash ../../TriPepSVM.sh -i ../data/proteome_9606.fasta -o ../data/cross_species/590_on_9606_proteome -id 590 -pos 1.8 -neg 0.2 -m trainData
@@ -11,7 +11,7 @@ mkdir ../data/cross_species/9606_on_590_proteome
 bash ../../TriPepSVM.sh -i ../data/proteome_99287.fasta -o ../data/cross_species/9606_on_590_proteome -id 9606 -pos 1.8 -neg 0.2 -m trainData
 
 mkdir ../data/cross_species/561_on_590_proteome
-bash ../../TriPepSVM.sh -i ../data/proteome_99287.fasta -o ../data/cross_species/561_on_590_proteome -id 561 -pos 1.8 -neg 0.2 -m trainData
+bash ../../TriPepSVM.sh -i ../data/proteome_99287.fasta -o ../data/cross_species/561_on_590_proteome -id 561 -pos 1.0 -neg 0.1 -m trainData
 
 # evaluate on e.coli proteome (for human and salmonella)
 mkdir ../data/cross_species/9606_on_561_proteome
